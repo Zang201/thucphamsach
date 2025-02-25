69ee554beb65c25b9f2c22202e71cc6bf3110a3f
nhatthao2k2 <nguyenthinhatthao1994@gmail.com>
2025-02-25 19:56:58 +0700

    Update app_master_admin.blade.php

--- resources/views/layouts/app_master_admin.blade.php
+++ resources/views/layouts/app_master_admin.blade.php
@@ -48,7 +48,40 @@
                 <!-- Header Navbar: style can be found in header.less -->
                 <nav class="navbar navbar-static-top">
                     <!-- Sidebar toggle button-->
-                   //////////////
+                   <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    </a>
+                    <div class="navbar-custom-menu">
+                        <ul class="nav navbar-nav">
+
+                            <li class="dropdown user user-menu">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <img src="{{ pare_url_file(get_data_user('admins','avatar')) }}" class="user-image" alt="User Image">
+                                <span class="hidden-xs">{{ get_data_user('admins','name') }}</span>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <!-- User image -->
+                                    <li class="user-header">
+                                        <img src="{{ pare_url_file(get_data_user('admins','avatar')) }}" class="img-circle" alt="User Image">
+                                        <p>
+                                            {{ get_data_user('admins','name') }}
+                                        </p>
+                                    </li>
+                                    <!-- Menu Footer-->
+                                    <li class="user-footer">
+                                        <div class="pull-left">
+                                            <a href="{{ route('admin.profile.index') }}" class="btn btn-default btn-flat">Profile</a>
+                                        </div>
+
+                                        <div class="pull-right">
+                                            <a href="{{ route('get.logout.admin') }}" class="btn btn-default btn-flat">Đăng xuất</a>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </li>
                             <!-- Control Sidebar Toggle Button -->
 {{--                            <li>--}}
 {{--                                <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>--}}
