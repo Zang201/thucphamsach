9501e1c25eeba027a4e61bf4a47bca77319d20ea
nhatthao2k2 <nguyenthinhatthao1994@gmail.com>
2025-03-10 18:49:39 +0700

    Update index.blade.php

--- resources/views/admin/menu/index.blade.php
+++ resources/views/admin/menu/index.blade.php
@@ -1,26 +1,32 @@
 @extends('layouts.app_master_admin')
+
 @section('content')
-    <!-- Content Header (Page header) -->
+    <!-- Tiêu đề trang -->
     <section class="content-header">
         <h1>Quản lý menu</h1>
         <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li><a href="{{  route('admin.menu.index') }}"> Menu</a></li>
-            <li class="active"> List </li>
+            <li><a href="{{ route('admin.menu.index') }}">Menu</a></li>
+            <li class="active">List</li>
         </ol>
     </section>
-    <!-- Main content -->
+
+    <!-- Nội dung chính -->
     <section class="content">
-        <!-- Default box -->
         <div class="box">
             <div class="box-header with-border">
                 <div class="box-header">
-                    <h3 class="box-title"><a href="{{ route('admin.menu.create') }}" class="btn btn-primary">Thêm mới <i class="fa fa-plus"></i></a></h3>
-               </div>
+                    <!-- Nút thêm mới menu -->
+                    <h3 class="box-title">
+                        <a href="{{ route('admin.menu.create') }}" class="btn btn-primary">
+                            Thêm mới <i class="fa fa-plus"></i>
+                        </a>
+                    </h3>
+                </div>
                 <div class="box-body">
-                   <div class="col-md-12">
+                    <div class="col-md-12">
                         <table class="table">
-                            <tbody>
+                            <thead>
                                 <tr>
                                     <th style="width: 10px">#</th>
                                     <th>Tên</th>
@@ -30,8 +36,10 @@
                                     <th>Ngày tạo</th>
                                     <th>Hành động</th>
                                 </tr>
+                            </thead>
+                            <tbody>
                                 @if ($menus)
-                                    @foreach($menus as $key => $menu)
+                                    @foreach($menus as $menu)
                                         <tr>
                                             <td>{{ $menu->id }}</td>
                                             <td>{{ $menu->mn_name }}</td>
@@ -39,23 +47,26 @@
                                                 <img src="{{ pare_url_file($menu->mn_avatar) }}" style="width: 80px;height: 80px;">
                                             </td>
                                             <td>
-                                                @if ($menu->mn_status == 1)
-                                                    <a href="{{ route('admin.menu.active', $menu->id) }}" class="label label-info">Show</a>
-                                                @else 
-                                                    <a href="{{ route('admin.menu.active', $menu->id) }}" class="label label-default">Hide</a>
-                                                @endif
+                                                <a href="{{ route('admin.menu.active', $menu->id) }}" 
+                                                   class="label {{ $menu->mn_status == 1 ? 'label-info' : 'label-default' }}">
+                                                    {{ $menu->mn_status == 1 ? 'Show' : 'Hide' }}
+                                                </a>
                                             </td>
                                             <td>
-                                                @if ($menu->mn_hot == 1)
-                                                    <a href="{{ route('admin.menu.hot', $menu->id) }}" class="label label-info">Hot</a>
-                                                @else 
-                                                    <a href="{{ route('admin.menu.hot', $menu->id) }}" class="label label-default">None</a>
-                                                @endif
+                                                <a href="{{ route('admin.menu.hot', $menu->id) }}" 
+                                                   class="label {{ $menu->mn_hot == 1 ? 'label-info' : 'label-default' }}">
+                                                    {{ $menu->mn_hot == 1 ? 'Hot' : 'None' }}
+                                                </a>
                                             </td>
-                                            <td>{{  $menu->created_at }}</td>
+                                            <td>{{ $menu->created_at }}</td>
                                             <td>
-                                                <a href="{{ route('admin.menu.update', $menu->id) }}" class="btn btn-xs btn-primary"><i class="fa fa-pencil"></i> Edit</a>
-                                                <a href="{{  route('admin.menu.delete', $menu->id) }}" class="btn btn-xs btn-danger js-delete-confirm"><i class="fa fa-trash"></i> Delete</a>
+                                                <a href="{{ route('admin.menu.update', $menu->id) }}" class="btn btn-xs btn-primary">
+                                                    <i class="fa fa-pencil"></i> Edit
+                                                </a>
+                                                <a href="{{ route('admin.menu.delete', $menu->id) }}" 
+                                                   class="btn btn-xs btn-danger js-delete-confirm">
+                                                    <i class="fa fa-trash"></i> Delete
+                                                </a>
                                             </td>
                                         </tr>
                                     @endforeach
@@ -65,7 +76,6 @@
                     </div>
                 </div>
             </div>
-            <!-- /.box -->
+        </div>
     </section>
-    <!-- /.content -->
 @stop
