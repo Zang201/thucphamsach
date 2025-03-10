eca7dd05dc7822187de0fd2dd05d6c5a419f9063
nhatthao2k2 <nguyenthinhatthao1994@gmail.com>
2025-03-10 18:57:14 +0700

    Update detail_order.blade.php

--- resources/views/admin/order/detail_order.blade.php
+++ resources/views/admin/order/detail_order.blade.php
@@ -1,63 +1,54 @@
 @extends('layouts.app_master_admin')
+
 @section('content')
-    <!-- Content Header (Page header) -->
+    <!-- Tiêu đề trang -->
     <section class="content-header">
         <h1>Quản lý đơn hàng</h1>
         <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li><a href="{{  route('xemdonhang') }}"> Đơn hàng</a></li>
-            <li class="active"> Chi tiết đơn hàng</li>
+            <li><a href="{{ route('xemdonhang') }}">Đơn hàng</a></li>
+            <li class="active">Chi tiết đơn hàng</li>
         </ol>
     </section>
-    <!-- Main content -->
+
+    <!-- Nội dung chính -->
     <section class="content">
-        <!-- Default box -->
         <div class="box">
             <div class="box-header with-border">
-              
                 <div class="box-body">
-                   <div class="col-md-12">
-                        <table class="table">
-                            <tbody>
+                    <div class="col-md-12">
+                        <table class="table table-bordered">
+                            <thead>
                                 <tr>
-                                    
                                     <th>Tên sản phẩm</th>
                                     <th>Số lượng</th>
                                     <th>Giá tiền</th>
                                     <th>Thành tiền</th>
-                                  
                                 </tr>
+                            </thead>
+                            <tbody>
+                                @php $total = 0; @endphp
+                                @foreach($order as $or)
                                     @php
-                                    $total = 0;
+                                        $subtotal = $or->od_qty * $or->product->pro_price;
+                                        $total += $subtotal;
                                     @endphp
-                                    @foreach($order as $key => $or)
-                                        @php
-
-                                            $subtotal = $or->od_qty*$or->product->pro_price;
-                                            $total+=$subtotal;
-                                        @endphp
-                                        <tr>
-                                            <td>{{ $or->product->pro_name }}</td>
-                                            <td>{{ $or->od_qty }}</td>
-                                            <td>{{  number_format($or->product->pro_price,0,',','.') }}đ</td>
-                                            <td>{{ number_format($subtotal,0,',','.') }}đ</td>
-                                           
-                                          
-                                        </tr>
-
-                                    @endforeach
                                     <tr>
-                                        <td colspan="3">Tổng tiền thanh toán : {{number_format($total,0,',','.')}}đ</td>
+                                        <td>{{ $or->product->pro_name }}</td>
+                                        <td>{{ $or->od_qty }}</td>
+                                        <td>{{ number_format($or->product->pro_price, 0, ',', '.') }}đ</td>
+                                        <td>{{ number_format($subtotal, 0, ',', '.') }}đ</td>
                                     </tr>
-                              
+                                @endforeach
+                                <tr>
+                                    <td colspan="3" class="text-right"><strong>Tổng tiền thanh toán:</strong></td>
+                                    <td><strong>{{ number_format($total, 0, ',', '.') }}đ</strong></td>
+                                </tr>
                             </tbody>
                         </table>
                     </div>
                 </div>
-               
-               
             </div>
-            <!-- /.box -->
+        </div>
     </section>
-    <!-- /.content -->
 @stop
