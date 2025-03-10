a5c546af2a05202897ee9a0f3a1ce980a4fb8ab0
buutran31 <languyenbuutran0@gmail.com>
2025-03-10 18:47:16 +0700

    Update index.blade.php

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -1,23 +1,26 @@
 @extends('layouts.app_master_admin')
+
 @section('content')
     <!-- Content Header (Page header) -->
     <section class="content-header">
         <h1>Quản lý thành viên</h1>
         <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li><a href="{{  route('admin.user.index') }}"> User</a></li>
-            <li class="active"> List</a></li>
+            <li><a href="{{ route('admin.user.index') }}"> User</a></li>
+            <li class="active"> List</li>
         </ol>
     </section>
+
     <!-- Main content -->
     <section class="content">
         <!-- Default box -->
         <div class="box">
             <div class="box-header with-border">
                 <div class="box-body">
-                   <div class="col-md-12">
+                    <div class="col-md-12">
+                        <!-- Table to display users -->
                         <table class="table">
-                            <tbody>
+                            <thead>
                                 <tr>
                                     <th style="width: 10px">#</th>
                                     <th>Name</th>
@@ -26,7 +29,9 @@
                                     <th>Time</th>
                                     <th>Action</th>
                                 </tr>
-                                @if (isset($users))
+                            </thead>
+                            <tbody>
+                                @if (isset($users) && $users->count() > 0)
                                     @foreach($users as $user)
                                         <tr>
                                             <td>{{ $user->id }}</td>
@@ -35,23 +40,36 @@
                                             <td>{{ $user->phone }}</td>
                                             <td>{{ $user->created_at }}</td>
                                             <td>
-                                                {{-- <a href="{{ route('admin.user.update', $user->id) }}" class="btn btn-xs btn-primary"><i class="fa fa-pencil"></i> Edit</a> --}}
-                                                <a href="{{  route('admin.user.delete', $user->id) }}" class="btn btn-xs btn-danger js-delete-confirm"><i class="fa fa-trash"></i> Delete</a>
+                                                <!-- Delete button with CSRF protection -->
+                                                <form action="{{ route('admin.user.delete', $user->id) }}" method="POST" style="display:inline;">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-xs btn-danger js-delete-confirm" aria-label="Delete">
+                                                        <i class="fa fa-trash"></i> Delete
+                                                    </button>
+                                                </form>
                                             </td>
                                         </tr>
                                     @endforeach
+                                @else
+                                    <!-- Display message if no users are available -->
+                                    <tr>
+                                        <td colspan="6" class="text-center">No users found.</td>
+                                    </tr>
                                 @endif
                             </tbody>
                         </table>
                     </div>
                 </div>
                 <!-- /.box-body -->
+
                 <div class="box-footer">
                     {!! $users->links() !!}
                 </div>
-                <!-- /.box-footer-->
+                <!-- /.box-footer -->
             </div>
             <!-- /.box -->
+        </div>
     </section>
     <!-- /.content -->
 @stop
