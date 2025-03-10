348a2f6e5561271f7ea6d81669a28c44c154c43f
nhatthao2k2 <nguyenthinhatthao1994@gmail.com>
2025-03-10 18:55:10 +0700

    Update update.blade.php

--- resources/views/admin/menu/update.blade.php
+++ resources/views/admin/menu/update.blade.php
@@ -1,45 +1,50 @@
 @extends('layouts.app_master_admin')
+
 @section('content')
-    <!-- Content Header (Page header) -->
+    <!-- Tiêu đề trang -->
     <section class="content-header">
-        <h1>Cập nhật mới menu</h1>
+        <h1>Cập nhật menu</h1>
         <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li><a href="{{  route('admin.menu.index') }}"> Menu</a></li>
-            <li class="active"> Update</a></li>
+            <li><a href="{{ route('admin.menu.index') }}">Menu</a></li>
+            <li class="active">Update</li>
         </ol>
     </section>
-    <!-- Main content -->
+
+    <!-- Nội dung chính -->
     <section class="content">
-        <!-- Default box -->
         <div class="box">
             <div class="box-header with-border">
                 <div class="box-body">
+                    <!-- Form cập nhật menu -->
                     <form role="form" action="" method="POST">
-                         @csrf
-                        <div class="col-sm-8">
-                            <div class="form-group {{ $errors->first('mn_name') ? 'has-error' : '' }}">
-                                <label for="name">Tên <span class="text-danger">(*)</span></label>
-                                <input type="text" class="form-control" value="{{ $menu->mn_name }}" name="mn_name"  placeholder="Name ...">
-                                @if ($errors->first('mn_name'))
-                                    <span class="text-danger">{{ $errors->first('mn_name') }}</span>
-                                @endif
+                        @csrf <!-- Token bảo vệ chống CSRF -->
+                        
+                        <div class="row">
+                            <div class="col-sm-8">
+                                <div class="form-group {{ $errors->has('mn_name') ? 'has-error' : '' }}">
+                                    <label for="mn_name">Tên <span class="text-danger">(*)</span></label>
+                                    <input type="text" class="form-control" value="{{ $menu->mn_name }}" name="mn_name" placeholder="Nhập tên menu...">
+                                    @if ($errors->has('mn_name'))
+                                        <span class="text-danger">{{ $errors->first('mn_name') }}</span>
+                                    @endif
+                                </div>
                             </div>
+                            <div class="col-sm-4"></div>
                         </div>
-                        <div class="col-sm-4">
-                            
+                        
+                        <!-- Nút thao tác -->
+                        <div class="box-footer text-center">
+                            <a href="{{ route('admin.menu.index') }}" class="btn btn-danger">
+                                Quay lại <i class="fa fa-undo"></i>
+                            </a>
+                            <button type="submit" class="btn btn-success">
+                                Cập nhật <i class="fa fa-save"></i>
+                            </button>
                         </div>
-                        <div class="col-sm-12">
-                            <div class="box-footer text-center">
-                                <a href="{{ route('admin.menu.index') }}" class="btn btn-danger">
-                                Quay lại <i class="fa fa-undo"></i></a>
-                                <button type="submit" class="btn btn-success">Cập nhật <i class="fa fa-save"></i></button>
-                            </div>
-                        </div>
-                    </form>  
+                    </form>
                 </div>
             </div>
-            <!-- /.box -->
+        </div>
     </section>
-    <!-- /.content -->
 @stop
