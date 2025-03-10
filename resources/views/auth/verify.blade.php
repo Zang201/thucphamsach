3de0f6c2d007a1aed2d0c961aa64795d3310b8e1
buutran31 <languyenbuutran0@gmail.com>
2025-03-10 19:04:23 +0700

    Update verify.blade.php

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,20 +5,23 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('Xác nhận địa chỉ email của bạn') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('Một liên kết xác minh mới đã được gửi đến email của bạn.') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    <p>{{ __('Trước khi tiếp tục, vui lòng kiểm tra email của bạn để tìm liên kết xác minh.') }}</p>
+                    <p>{{ __('Nếu bạn không nhận được email') }},</p>
+
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">
+                            {{ __('nhấn vào đây để yêu cầu gửi lại') }}
+                        </button>.
                     </form>
                 </div>
             </div>
